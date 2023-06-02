57d526f9f33e70e8723973c1fda4629c895407f2
mowu4mew <sns_youcanreally@icloud.com>
2023-06-02 15:46:18 +0900

    update wings42 qwerty

--- keyboards/dailycraft/wings42/rev2/keymaps/qwerty/keymap.c
+++ keyboards/dailycraft/wings42/rev2/keymaps/qwerty/keymap.c
@@ -87,7 +87,7 @@ const key_override_t undssft_key_override = ko_make_basic(MOD_MASK_SHIFT, MT_CTL
 const key_override_t dquosft_key_override = ko_make_basic(MOD_MASK_SHIFT, JP_QUOT, JP_DQUO);    //"[SHIFT & JP_QUOT]
 const key_override_t colnsft_key_override = ko_make_basic(MOD_MASK_SHIFT, JP_SCLN, JP_COLN);    //:[SHIFT & JP_SCLN]
 const key_override_t tildsft_key_override = ko_make_basic(MOD_MASK_SHIFT, JP_TILD, JP_GRV);     //`[SHIFT & JP_TILD]
-const key_override_t yensft_key_override = ko_make_basic(MOD_MASK_SHIFT, JP_YEN, JP_PoiIPE);      //\[SHIFT & JP_YEN]
+const key_override_t yensft_key_override = ko_make_basic(MOD_MASK_SHIFT, JP_YEN, JP_PIPE);      //\[SHIFT & JP_YEN]
 
 const key_override_t **key_overrides = (const key_override_t *[]){
   &undssft_key_override,
