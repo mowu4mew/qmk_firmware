a510914ad536eb22a0c0d44d537feb60552d58be
mowu4mew <sns_youcanreally@icloud.com>
2024-08-22 18:17:55 +0900

    update wings

--- keyboards/dailycraft/wings42/rev2/keymaps/qwerty/keymap.c
+++ keyboards/dailycraft/wings42/rev2/keymaps/qwerty/keymap.c
@@ -484,15 +484,15 @@ void pointing_device_init_user(void) {
 bool encoder_update_user(uint8_t index, bool clockwise) {
     if (index == 0) { /* First encoder */
         if (clockwise) {
-            tap_code(KC_PGDN);
-        } else {
-            tap_code(KC_PGUP);
-        }
-    } else if (index == 1) { /* Second encoder */
-        if (clockwise) {
             tap_code(KC_DOWN);
         } else {
             tap_code(KC_UP);
+        }
+    } else if (index == 1) { /* Second encoder */
+        if (clockwise) {
+            tap_code(KC_PGDN);
+        } else {
+            tap_code(KC_PGUP);
         }
     }
     return false;
