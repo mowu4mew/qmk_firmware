7007f7352a9a64b55e0a4ddf3b68ad1ea2cc7bf3
mowu4mew <sns_youcanreally@icloud.com>
2025-03-31 21:56:47 +0900

    update crkbd

--- keyboards/crkbd/keymaps/mowu4mew/keymap.c
+++ keyboards/crkbd/keymaps/mowu4mew/keymap.c
@@ -88,7 +88,7 @@ const key_override_t colnsft_key_override = ko_make_basic(MOD_MASK_SHIFT, JP_SCL
 const key_override_t tildsft_key_override = ko_make_basic(MOD_MASK_SHIFT, JP_TILD, JP_GRV);     //`[SHIFT & JP_TILD]
 const key_override_t yensft_key_override = ko_make_basic(MOD_MASK_SHIFT, JP_YEN, JP_PIPE);      //\[SHIFT & JP_YEN]
 
-const key_override_t **key_overrides = (const key_override_t *[]){
+const key_override_t *key_overrides[] = {
   &undssft_key_override,
   &dquosft_key_override,
   &colnsft_key_override,
@@ -191,7 +191,7 @@ uint16_t get_tapping_term(uint16_t keycode, keyrecord_t *record) {
     case SFT_PST:
       return 400;
     case TD_Q_ESC:
-      return 60;   //短くするとタップになりやすい、長いとダブルタップになりやすい
+      return 400;   //短くするとタップになりやすい、長いとダブルタップになりやすい
     default:
       return TAPPING_TERM;
   }
