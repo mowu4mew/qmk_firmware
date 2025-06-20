f85877f8385e64e3fcef8b169728cdffc6073875
mowu4mew <greedispoison.656@icloud.com>
2025-06-20 16:37:56 +0900

    update wings

--- keyboards/dailycraft/wings42/rev2/keymaps/qwerty/config.h
+++ keyboards/dailycraft/wings42/rev2/keymaps/qwerty/config.h
@@ -12,7 +12,6 @@
 
 #define TAPPING_TERM 200
 #define TAPPING_TERM_PER_KEY
-#define IGNORE_MOD_TAP_INTERRUPT    //タップ優先
 
 //#define PERMISSIVE_HOLD           //MTキーホールド判定前に次キーtapでもmod有効
 //#define PERMISSIVE_HOLD_PER_KEY
